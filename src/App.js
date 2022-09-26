6926e158f76d54c88dfb9fec4dc9878e82639764
Oma Withers <omacwitherssei@gmail.com>
2022-09-25 23:12:26 -0400

    "Added WebsiteVisitors"

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import ReactStaticComponents from "./components/ReactStaticComponents"
 import Reviews from "./components/Reviews";
 import SentimentAnalysis from "./components/SentimentAnalysis";
 import AverageRating from "./components/AverageRating";
-
+import WebsiteVisitors from "./components/WebsiteVisitors";
 import './App.css';
 
 
@@ -14,8 +14,10 @@ function App() {
       <Reviews reviewNum={1281} />
       <SentimentAnalysis />
       <AverageRating />
+      <WebsiteVisitors />
+      </div>
     </div>
-    </div>
+
   );
 }
 export default App;
---
+++ src/components/WebsiteVisitors.js
@@ -0,0 +1,9 @@
+function WebsiteVisitors() {
+    return (
+        <div id='WebsiteVisitors'>
+<h4>Website Visitors</h4>
+<h1>821</h1>
+        </div>
+    );
+}
+export default WebsiteVisitors;
